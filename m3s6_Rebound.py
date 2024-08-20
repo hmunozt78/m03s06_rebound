48d9227e6593f7eda7146336e83523387e32fd74
Hugo Muñoz <hmunoz@test.edu>
2024-08-19 20:07:03 -0400

    Primer Commit

---
+++ m3s6_Rebound.py
@@ -0,0 +1,17 @@
+numero = 0
+
+if numero!=0:
+    if (numero%2)>0:
+        if numero>0:
+            print(f"el numero {numero} es impar positivo")
+        else:
+           print(f"El numero {numero} es impar negativo")
+    else:
+        if numero>0:
+            print(f"el numero {numero} es par positivo")
+        else:
+            print(f"el numero {numero} es par negativo")
+        
+else:
+    print("el numero es CERO")
+    
---
+++ readme.txt
@@ -0,0 +1,7 @@
+Modulo 03
+Sesión 06
+Ejercicio Comprobacion
+
+Para obtener copia de este proyecto, ejecute el siguiente comando:
+
+git clone https://github.com/hmunozt78/m04s06_rebound.git
